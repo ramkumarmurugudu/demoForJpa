a0bd0513fc48fdccee23dbc8c3f527e79d989492
ramkumar <itsmeramkumar.ed>
2025-10-07 01:12:57 +0530

    test

--- src/main/java/com/example/demoForJpa/entity/Student.java
+++ src/main/java/com/example/demoForJpa/entity/Student.java
@@ -1,48 +1,60 @@
 package com.example.demoForJpa.entity;
 
-import javax.persistance.Entity;
-import javax.persistance.GeneratedValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
-	public class Student {
-	private long id;
-	private String studentName;
-	private String studentEmail;
-	private String studentAddress;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getStudentName() {
-		return studentName;
-	}
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
-	public String getStudentEmail() {
-		return studentEmail;
-	}
-	public void setStudentEmail(String studentEmail) {
-		this.studentEmail = studentEmail;
-	}
-	public String getStudentAddress() {
-		return studentAddress;
-	}
-	public void setStudentAddress(String studentAddress) {
-		this.studentAddress = studentAddress;
-	}
-	public Student() {
-		super();
-		
-	}
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", studentName=" + studentName + ", studentEmail=" + studentEmail + ", studentAddress="
-				+ studentAddress + "]";
-	}
+public class Student {
 
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
+    private String studentName;
+    private String studentEmail;
+    private String studentAddress;
+
+    public Student() {
+        super();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", studentName=" + studentName + ", studentEmail=" + studentEmail
+                + ", studentAddress=" + studentAddress + "]";
+    }
 }
